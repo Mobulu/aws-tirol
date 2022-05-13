6d63cc0645aab1542d442f44c458bf14d4ab5244
Mario Lehmeyer <mario.lehmeyer@student.uibk.ac.at>
2022-05-13 16:52:09 +0200

    Beautify

--- main.js
+++ main.js
@@ -60,25 +60,22 @@ async function loadData(url) {
     let response = await fetch(url);
     let geojson = await response.json();
 
-// Wetterstationen mit Icons und Popups implementieren
-
-L.geoJSON(geojson, {
-    pointToLayer: function (geoJsonPoint, latlng) {
-        //L.marker(latlng).addTo(map)
-        //console.log(geoJsonPoint.properties.NAME);
-        let popup = `
+    // Wetterstationen mit Icons und Popups implementieren
+    L.geoJSON(geojson, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            let popup = `
             <strong>${geoJsonPoint.properties.name}</strong>
             <p>(${geoJsonPoint.geometry.coordinates[2]})</p>
        `;
-        return L.marker(latlng, {
-            icon: L.icon({
-                iconUrl: "icons/wifi.png",
-                iconAnchor: [16, 37],
-                popupAnchor: [0, -37]
-            })
-        }).bindPopup(popup);
-    }
-}).addTo(map);
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: "icons/wifi.png",
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -37]
+                })
+            }).bindPopup(popup);
+        }
+    }).addTo(map);
 
 }
 loadData("https://static.avalanche.report/weather_stations/stations.geojson");
